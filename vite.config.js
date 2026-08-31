0d6eec619def300eab706ed926f7aab096a8bd52
Gabriel Viana Santos <gabriel.viana.gv02@gmail.com>
2026-08-31 20:45:13 -0300

    Fix: Ajuste configurações vite

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        leadQualification: resolve(__dirname, 'lead-qualification.html')
+        main: resolve(__dirname, 'index.html')
       }
     }
   }
